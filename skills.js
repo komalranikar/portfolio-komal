225e81a6a5ce80dc5aa45aa8ee3e82a982aa1b6b
Komal Rani Kar <93367596+komalranikar@users.noreply.github.com>
2024-08-06 12:05:08 +0530

    Create skills.js

---
+++ skills.js
@@ -0,0 +1,35 @@
+[
+    {
+        "name": "Java",
+        "icon": "https://raw.githubusercontent.com/jmnote/z-icons/master/svg/java.svg"
+    },
+    {
+        "name": "Python",
+        "icon": "https://raw.githubusercontent.com/jmnote/z-icons/master/svg/python.svg"
+    },
+    {
+        "name": "HTML5",
+        "icon": "https://raw.githubusercontent.com/bablubambal/All_logo_and_pictures/1ac69ce5fbc389725f16f989fa53c62d6e1b4883/social%20icons/html5.svg"
+    },
+    {
+        "name": "CSS",
+        "icon": "https://raw.githubusercontent.com/bablubambal/All_logo_and_pictures/1ac69ce5fbc389725f16f989fa53c62d6e1b4883/social%20icons/css3.svg"
+    },
+    {
+        "name": "JavaScript",
+        "icon": "https://raw.githubusercontent.com/bablubambal/All_logo_and_pictures/1ac69ce5fbc389725f16f989fa53c62d6e1b4883/social%20icons/javascript.svg"
+    },
+    {
+        "name": "Bootstrap",
+        "icon": "https://img.icons8.com/color/48/000000/bootstrap.png"
+    },
+    {
+        "name": "Streamlit",
+        "icon": "https://raw.githubusercontent.com/bablubambal/All_logo_and_pictures/7c0ac2ceb9f9d24992ec393d11fa7337d2f92466/frameworks/nodejs.svg"
+    },
+   
+    {
+        "name": "MySQL",
+        "icon": "https://www.svgrepo.com/show/303251/mysql-logo.svg"
+    }
+]
